83e2e039992ceb2093fe7d6fcd1ae91ef10784ea
Gregory Maldonado <gmaldona@oswego.edu>
2020-03-23 00:05:53 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,21 @@
+import time
+import os
+import sys
+from threading import Thread
+
+def run():
+    os.system('python3 VirusTracker.py')
+
+def Main():
+    while True:
+        os.system('cd')
+        os.system('cd Desktop/COVID-19_Update')
+        os.system(sys.exit())
+        t1 = Thread(target=run)
+        t1.start()
+        sleep(10)
+        exit()
+        print('program terminated')
+
+if __name__ == '__run__':
+    Main()
